ce6dfbeb6e36d726c47b3c6561ec6133ab06f284
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-09-02 09:28:34 +0200

    Completede

---
+++ src/main/java/com/example/springaddtwonumbers/SpringAddTwoNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaddtwonumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddTwoNumbersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAddTwoNumbersApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springaddtwonumbers/SpringAddTwoNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaddtwonumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddTwoNumbersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
